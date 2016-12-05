7f6aab4d3b2d44fefbdb30d14f04fdd230605c63
Foggy <Finder>
2016-12-05 22:36:29 +0200

    ...

---
+++ GjhMCVE/GjhMCVE/App.fs
@@ -0,0 +1,20 @@
+﻿module main
+
+open System
+open FsXaml
+open System.Windows
+
+type MainWindow = XAML<"MainWindow.xaml">
+
+[<STAThread>]
+[<EntryPoint>]
+let main argv =
+    let app = Application()
+
+    Gjallarhorn.Wpf.Platform.install true
+    |> ignore
+
+    let vm = ViewModels.Context.create()
+    let window = MainWindow(DataContext = vm)
+
+    app.Run window
---
+++ GjhMCVE/GjhMCVE/MainWindow.xaml.fs
@@ -0,0 +1,43 @@
+﻿namespace ViewModels
+
+open System
+open System.Windows
+
+open FsXaml
+
+module Context =
+
+    open Gjallarhorn
+    open Gjallarhorn.Bindable
+
+    let rand = Random()
+    let choose n = 
+        Array.sortBy (fun _ -> rand.Next()) [|0..10|]
+
+    let create () =    
+        let source = Binding.createSource()
+
+        let count = 10
+        let rand = Random()
+        let model : IMutatable<int[]> = Mutable.create [| |]
+                 
+        let up items param = 
+            //Do something with items
+            printfn "%A" items
+            items
+
+        Binding.toView source "Model" model
+
+        Binding.createCommand "CreateCommand" source
+        |> Observable.subscribe 
+            (fun _ -> choose count |> Mutable.set model)
+        |> source.AddDisposable
+
+        Binding.createMessageParam "Up" (up model.Value) source
+        |> Observable.subscribe (Mutable.set model)
+
+//        Binding.createMessageParam "Up" id source
+//        |> Observable.subscribe (fun p -> Mutable.set model (up model.Value p))
+        |> source.AddDisposable
+
+        source
